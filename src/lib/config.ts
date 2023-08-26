544bcce759ed674106d27aa6bedc8e38da1e7098
ACHii <>
2023-08-26 16:13:54 +0530

    basic setup

---
+++ src/lib/config.ts
@@ -0,0 +1,5 @@
+import { dev } from '$app/environment'
+
+export const title = 'Sveltekit Blog'
+export const description = 'A Sveltekit blog for ...'
+export const url = dev ? 'http://localhost:5173/' : 'http://blog.com/'
